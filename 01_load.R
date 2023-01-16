3c1d158928f810299d15163130c62b9c1e8f790c
Chris Madsen <78942676+chrispmad@users.noreply.github.com>
2023-01-16 12:45:05 -0800

    added Carl's code to read, clean and investigate single station's data

--- 01_load.R
+++ 01_load.R
@@ -1,12 +1,55 @@
 # Copyright 2023 Province of British Columbia
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 # http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
+
+library(StreamFlowTrend)
+# library(broom)
+# library(broom.mixed)
+# library(nlme)
+library(tidyverse)
+library(tidyhydat)
+library(magrittr)
+# library(plotly)
+
+# The following code is from Carl's StreamFlowTrend work (script='v01-individual-station-analysis')
+
+trend.stat.names <- c("estimate","se","p.value","rho","se.adj","p.value.adj")
+
+# Picking single station to develop data access pipeline
+station_number <- '08NM116'
+station.info <- hy_stations( station_number)
+
+# get the bc_stations data
+bc_stations <- hy_stations( prov_terr_state_loc="BC")
+
+# What are available statistics?
+stfl_get_avail_stat()
+
+# Note: avoid the maximum/minimum statistics
+
+hydata <- tidyhydat::hy_daily_flows(station_number = station_number)
+hydata$Year <- lubridate::year(hydata$Date)
+temp <- as.data.frame(xtabs(~Year, data=hydata, exclude=NULL, na.action=na.pass))
+temp$Year <- as.numeric(as.character(temp$Year)) # convert from factor
+temp$Decade <- floor(temp$Year/10)*10
+temp$Year.in.Decade = temp$Year %% 10
+xtabs(Freq ~ Decade+Year.in.Decade, data=temp)
+
+# We see that for this station, complete (or nearly complete) daily data is only
+# available starting around 1967. Caution will be needed to interpret any
+# long-term trends that include data from 1949 to 1965 that excludes about
+# 1/2 of the year.
+
+# get the mean daily flow
+station.mean <- stfl_get_annual_stat(station_number, Parameter="Flow", Statistic=c("MEAN"), ignore_missing=TRUE)
+head(station.mean)
+tail(station.mean)
 
--- 03_analysis.R
+++ 03_analysis.R
@@ -1,12 +1,46 @@
 # Copyright 2023 Province of British Columbia
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 # http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
+library(EnvStats)
+
+# Least Squares trend line.
+lm.fit <- lm(Value ~ Year, data=station.mean)
+summary(lm.fit)$coefficients
+
+# Mann-Kendall trend test.
+mk.test <- kendallTrendTest(Value~Year, data=station.mean)
+mk.test
+cat("Confidence interval for the slope \n")
+mk.test$interval$limits
+
+# Tau is the MK correlation between the year and value variables.
+cor.test(station.mean$Value, station.mean$Year, method="kendall")
+
+# "Linear" trend line of Senn slope.
+station.mean$mk.pred <- mk.test$estimate["intercept"]+
+  mk.test$estimate["slope"]*station.mean$Year
+
+plot1 %>% add_trace(data=station.mean, x=~Year, y=~mk.pred, mode="lines")
+
+# Note: we may prefer to use a version of the MK test that accounts for
+# serial autocorrelation. This function shows the old and new p values and
+# other parameters of the model fit.
+mmky.test <- modifiedmk::mmky1lag(station.mean$Value)
+mmky.test
+
+# We could also use a 'robust' linear regression model.
+lmrob.fit <- robustbase::lmrob(Value ~ Year, data=station.mean)
+summary(lmrob.fit)$coefficients
+
+station.mean$lmrob.pred <- predict(lmrob.fit, newdata=station.mean)
+
+plot1 %>% add_trace(data=station.mean, x=~Year, y=~lmrob.pred, mode="lines")
