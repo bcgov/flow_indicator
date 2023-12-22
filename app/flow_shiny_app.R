dc082d518f21c110354ee89ef86a74ca5c0b7388
Andrew Ladle <andrew.ladle@gov.bc.ca>
2023-12-22 11:18:20 -0800

    Remove opacity

--- app/flow_shiny_app.R
+++ app/flow_shiny_app.R
@@ -15,8 +15,8 @@ source('functions.R')
 
 ui = shiny::fluidPage(
   tags$head(tags$style(
-    HTML('#trend_selector {opacity:0.5;}
-         #trend_selector:hover{opacity:0.9;}'))),
+    HTML('#trend_selector {opacity:0.9;}
+         #trend_selector:hover{opacity:1;}'))),
   titlePanel("Flow Indicator"),
   map_abs_panel,
   trend_select_abs_panel
